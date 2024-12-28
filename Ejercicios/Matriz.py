525532da31c94b912bf9d64a115ebfd7c757c493
zDefcon20 <antholozadarequelme@gmail.com>
2024-12-28 00:02:39 -0500

    update

---
+++ Ejercicios/Matriz.py
@@ -0,0 +1,14 @@
+matriz = [[0 for _ in range(3)] for _ in range(3)]
+
+# Ingresar los valores de la matriz
+for i in range(3):
+  for j in range(3):
+    valor = int(input(f"Ingrese el valor de la posicion [{i+1},{j+1}]: "))
+    matriz[i][j] = valor
+
+# Mostrar los valores de la matriz    
+print("Los valores de la matriz son: ")
+for fila in matriz:
+  for valor in fila:
+    print(valor, end=" ")
+  print()
