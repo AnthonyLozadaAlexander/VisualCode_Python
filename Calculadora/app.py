698a1c085267f41e7b8556aa669c20e9670d0a9d
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-18 17:06:35 -0500

    Update

---
+++ Calculadora/app.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+Window = Tk() #?Instancia para crear la ventana
+Window.title("Calculator") #?Asignarle titulo a la ventana
+Window.geometry("125x125")
+Window.eval('tk::PlaceWindow . center')
+
+display = Entry(Window) #?Crear un espacio de texto
+display.grid(row=1, columnspan=6, sticky=W+E) #*Creacion de la grid del espacio de la ventana
+
+#Row = fila, Column = columna
+#sticky = W+E -> Que se expanda de izquierda a derecha
+
+#Numeric Buttons
+Button(Window, text="1").grid(row=2, column=0, sticky=W+E)
+Button(Window, text="2").grid(row=2, column=1, sticky=W+E)
+Button(Window, text="3").grid(row=2, column=2, sticky=W+E)
+
+Button(Window, text="4").grid(row=3, column=0, sticky=W+E)
+Button(Window, text="5").grid(row=3, column=1, sticky=W+E)
+Button(Window, text="6").grid(row=3, column=2, sticky=W+E)
+
+Button(Window, text="7").grid(row=4, column=0, sticky=W+E)
+Button(Window, text="8").grid(row=4, column=1, sticky=W+E)
+Button(Window, text="9").grid(row=4, column=2, sticky=W+E)
+
+#Buttons Operations
+Button(Window, text="AC").grid(row=5, column=0, sticky=W+E)
+Button(Window, text="0").grid(row=5, column=1, sticky=W+E)
+Button(Window, text="%").grid(row=5, column=2, sticky=W+E)
+
+Button(Window, text="+").grid(row=2, column=3, sticky=W+E)
+Button(Window, text="-").grid(row=3, column=3, sticky=W+E)
+Button(Window, text="x").grid(row=4, column=3, sticky=W+E)
+Button(Window, text="/").grid(row=5, column=3, sticky=W+E)
+
+Window.mainloop() #?Mantener la ventana abierta
