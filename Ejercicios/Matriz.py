5e48e9425a10291cd92e5a74eb874baf952f639e
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-18 16:27:52 -0500

    agregar funcionalidad para ingresar y mostrar una matriz, así como calcular la suma de su diagonal principal

--- Ejercicios/Matriz.py
+++ Ejercicios/Matriz.py
@@ -1,17 +1,31 @@
 matriz = [[0 for _ in range(3)] for _ in range(3)]
 
 # Ingresar los valores de la matriz
-for i in range(3):
-  for j in range(3):
+for i in range(3): #? Se Ingresa primero a la fila
+  for j in range(3): #? Se recorren las columnas
+    
     valor = int(input(f"Ingrese el valor de la posicion [{i+1},{j+1}]: "))
     matriz[i][j] = valor
 
 # Mostrar los valores de la matriz    
-print("Los valores de la matriz son: ")
-for fila in matriz:
-  for valor in fila:
-    print(valor, end=" ")
+print("\nLos valores de la matriz son: \n")
+for i in range(3):
+  for j in range(3):
+    print(matriz[i][j], end=" ")
+    
   print()
+  
+# Sumar los valores de la diagonal principal
+suma = 0
+print("\nLa Diagonal Principal es: \n")
+for i in range(3):
+  for j in range(3):
+    if(i == j):
+      suma = suma + matriz[i][j]
+      print(matriz[i][j])
+      
+print("---------------------------------------------------------")
+print(f"La suma de la diagonal principal de la matriz es: {suma}")
   
 
   
--- Python-DesdeCero/ejercicioasd.py
+++ Python-DesdeCero/ejercicioasd.py
@@ -8,8 +8,8 @@ print()
 print(x != y) #? sirve para comparar si son "diferentes" y decirme si es true or false
 
 print()
-
-print("Calculo Factorial De Un Numero")
+#-----------------------------------------------------------------------------------------
+print("\nCalculo Factorial De Un Numero")
 print()
 
 n = int(input("Ingrese un numero: "))
@@ -21,7 +21,8 @@ for i in range (1, n+1):
 print(f"El factorial de {n} es {factorial}")
 
 print()
-print("Verificar si un numero es primo")
+#-----------------------------------------------------------------------------------------
+print("\nVerificar si un numero es primo")
 
 n1 = int(input("Ingrese un numero: "))
 esPrimo = True #? el valor por defecto es true
@@ -30,7 +31,7 @@ if n1 <= 1: #? si el numero ingresado es menor o igual a 1, entonces no puede se
   esPrimo = False #? si no es primo, sera false
 else:
   for i in range(2, n):
-    if n1 % 1 == 0:
+    if n1 % 1 == 0: #? si el residuo de la division entre 1 es 0, entonces no es primo
      esPrimo = False
      
 if esPrimo == True:
